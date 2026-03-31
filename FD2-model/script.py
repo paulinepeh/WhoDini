182530e1f0b206d2326cd6ccd1538a077743d644
peh ding hao pauline <pauline@pehs-MacBook-Pro.local>
2026-04-01 07:15:54 +0800

    Add FD2 face detection  with whitelist filtering, tracking, and video processing

---
+++ FD2-model/script.py
@@ -0,0 +1,259 @@
+import cv2
+import time
+import numpy as np
+from collections import defaultdict, deque
+from ultralytics import YOLO
+from insightface.app import FaceAnalysis
+import os
+
+
+SIM_THRESHOLD = 0.45
+FACE_REFRESH_INTERVAL = 2
+
+MOUTH_THRESHOLD = 0.025
+NOISE_THRESHOLD = 0.01
+SPEAK_FRAMES = 5
+MIN_FACE_RATIO = 0.03
+
+SMOOTH_ALPHA = 0.7
+
+# stability memory
+SPEAKING_HOLD_FRAMES = 15
+BLUR_DELAY_FRAMES = 10
+
+print("Loading models...")
+model = YOLO("../models/best.pt")
+
+face_app = FaceAnalysis(name="buffalo_l")
+face_app.prepare(ctx_id=0)
+whitelist = {}
+
+for file in os.listdir("whitelist"):
+    img = cv2.imread(f"whitelist/{file}")
+    if img is None:
+        continue
+
+    faces = face_app.get(img)
+    if len(faces) > 0:
+        emb = faces[0].embedding
+        whitelist.setdefault("pauline", []).append(emb)
+
+print("Whitelist loaded:", whitelist.keys())
+
+cap = cv2.VideoCapture("videos/video.mp4")
+
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+out = cv2.VideoWriter(
+    "output/output.mp4",
+    cv2.VideoWriter_fourcc(*"mp4v"),
+    fps,
+    (w, h)
+)
+# memory
+track_identity = {}
+track_score = {}
+track_boxes = {}
+prev_mouth_center = {}
+mouth_open_history = defaultdict(lambda: deque(maxlen=10))
+cached_faces = []
+
+speaking_memory = {}
+blur_delay_memory = {}
+
+frame_idx = 0
+prev_time = time.time()
+fps_display = 0
+
+print("Running...")
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    curr_time = time.time()
+    instant_fps = 1 / (curr_time - prev_time + 1e-6)
+    prev_time = curr_time
+    fps_display = 0.9 * fps_display + 0.1 * instant_fps
+# YOLO
+    results = model.track(frame, persist=True, conf=0.25, verbose=False)
+
+    if len(results) == 0 or results[0].boxes is None:
+        out.write(frame)
+        continue
+
+    boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
+    track_ids = results[0].boxes.id
+
+    if track_ids is not None:
+        track_ids = track_ids.cpu().numpy().astype(int)
+    else:
+        track_ids = [-1] * len(boxes)
+
+    smoothed = []
+    for box, tid in zip(boxes, track_ids):
+        x1, y1, x2, y2 = box
+
+        if tid in track_boxes:
+            px1, py1, px2, py2 = track_boxes[tid]
+            x1 = int(SMOOTH_ALPHA * px1 + (1 - SMOOTH_ALPHA) * x1)
+            y1 = int(SMOOTH_ALPHA * py1 + (1 - SMOOTH_ALPHA) * y1)
+            x2 = int(SMOOTH_ALPHA * px2 + (1 - SMOOTH_ALPHA) * x2)
+            y2 = int(SMOOTH_ALPHA * py2 + (1 - SMOOTH_ALPHA) * y2)
+
+        track_boxes[tid] = [x1, y1, x2, y2]
+        smoothed.append((x1, y1, x2, y2, tid))
+        # insightface
+    if frame_idx % FACE_REFRESH_INTERVAL == 0:
+        faces = face_app.get(frame)
+        cached_faces = []
+        for face in faces:
+            fx1, fy1, fx2, fy2 = face.bbox.astype(int)
+            cached_faces.append({
+                "bbox": [fx1, fy1, fx2, fy2],
+                "emb": face.embedding,
+                "kps": face.kps
+            })
+
+    for x1, y1, x2, y2, tid in smoothed:
+
+        identity = track_identity.get(tid, "unknown")
+        score = track_score.get(tid, 0)
+
+        best_face = None
+        best_iou = 0
+
+        for face in cached_faces:
+            fx1, fy1, fx2, fy2 = face["bbox"]
+
+            inter = max(0, min(x2, fx2) - max(x1, fx1)) * \
+                    max(0, min(y2, fy2) - max(y1, fy1))
+
+            if inter > best_iou:
+                best_iou = inter
+                best_face = face
+
+        is_speaking = False
+
+        if best_face is not None:
+            emb = best_face["emb"]
+            kps = best_face["kps"]
+
+            #recognition
+            best_sim = -1
+            best_name = "unknown"
+
+            for name, refs in whitelist.items():
+                for ref in refs:
+                    sim = np.dot(emb, ref) / (
+                        np.linalg.norm(emb) * np.linalg.norm(ref) + 1e-6
+                    )
+                    if sim > best_sim:
+                        best_sim = sim
+                        best_name = name
+
+            if best_sim > SIM_THRESHOLD:
+                identity = best_name
+                score = best_sim
+            else:
+                identity = "unknown"
+                score = best_sim
+
+            track_identity[tid] = identity
+            track_score[tid] = score
+
+            # speaker detection
+            if kps is not None:
+                left = kps[3]
+                right = kps[4]
+                mouth_center = (left + right) / 2
+
+                if tid in prev_mouth_center:
+                    prev = prev_mouth_center[tid]
+
+                    vertical = abs(mouth_center[1] - prev[1])
+                    face_h = (y2 - y1)
+
+                    if face_h > 0:
+                        vertical /= face_h
+
+                    if vertical < NOISE_THRESHOLD:
+                        vertical = 0
+
+                    speaking_frame = vertical > MOUTH_THRESHOLD
+                else:
+                    speaking_frame = False
+
+                prev_mouth_center[tid] = mouth_center
+                mouth_open_history[tid].append(speaking_frame)
+
+                sustained = sum(mouth_open_history[tid]) >= SPEAK_FRAMES
+
+                # distance filter
+                face_area = (x2 - x1) * (y2 - y1)
+                frame_area = frame.shape[0] * frame.shape[1]
+                close = (face_area / frame_area) > MIN_FACE_RATIO
+
+                # speaking memory
+                if sustained and close:
+                    speaking_memory[tid] = SPEAKING_HOLD_FRAMES
+                else:
+                    if tid in speaking_memory:
+                        speaking_memory[tid] -= 1
+
+                if speaking_memory.get(tid, 0) > 0:
+                    is_speaking = True
+
+      
+                lx, ly = int(left[0]), int(left[1])
+                rx, ry = int(right[0]), int(right[1])
+                cx, cy = int(mouth_center[0]), int(mouth_center[1])
+
+                cv2.circle(frame, (lx, ly), 3, (255, 0, 0), -1)
+                cv2.circle(frame, (rx, ry), 3, (255, 0, 0), -1)
+                cv2.circle(frame, (cx, cy), 5, (0, 255, 255), -1)
+
+        # stable blur
+        crop = frame[y1:y2, x1:x2]
+
+        if identity != "pauline":
+            if is_speaking:
+                blur_delay_memory[tid] = BLUR_DELAY_FRAMES
+                should_blur = False
+            else:
+                if tid in blur_delay_memory:
+                    blur_delay_memory[tid] -= 1
+
+                should_blur = blur_delay_memory.get(tid, 0) <= 0
+
+            if should_blur:
+                frame[y1:y2, x1:x2] = cv2.GaussianBlur(crop, (99, 99), 30)
+
+        if identity == "pauline":
+            color = (0, 255, 0)
+        elif is_speaking:
+            color = (255, 255, 0)
+        else:
+            color = (0, 0, 255)
+
+        label = f"{identity} | {score:.2f}"
+        if is_speaking:
+            label += " (speaking)"
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(frame, label, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+
+    cv2.putText(frame, f"FPS: {int(fps_display)}",
+                (20, 40), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 255, 255), 2)
+
+    out.write(frame)
+    frame_idx += 1
+
+cap.release()
+out.release()
+
+print("DONE: output.mp4")
